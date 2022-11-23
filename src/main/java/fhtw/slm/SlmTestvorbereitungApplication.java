a4fdf4bd27c0dcbc4c9633c17974cc3ea0f70145
juro97 <p.jurassovits@gmail.com>
2022-11-23 21:17:42 +0100

    Initial Commit

---
+++ src/main/java/fhtw/slm/SlmTestvorbereitungApplication.java
@@ -0,0 +1,12 @@
+package fhtw.slm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlmTestvorbereitungApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SlmTestvorbereitungApplication.class, args);
+    }
+}
---
+++ src/test/java/fhtw/slm/SlmTestvorbereitungApplicationTests.java
@@ -0,0 +1,13 @@
+package fhtw.slm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlmTestvorbereitungApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
