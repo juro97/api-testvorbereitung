cf746acaae3e4168d5475055ab9fdfca93b14bb6
juro97 <p.jurassovits@gmail.com>
2022-11-24 00:05:34 +0100

    (this commit will...) add CalculatorController and sum function

--- src/main/java/fhtw/slm/CalculatorController.java
+++ src/main/java/fhtw/slm/CalculatorController.java
@@ -1,6 +1,5 @@
 package fhtw.slm;
 
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +7,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CalculatorController {
 
-    @GetMapping("/sum") {
-        asd;
+    @RequestMapping("/sum")
+    public int sum(@RequestParam int a, @RequestParam int b){
+        return a + b;
     }
 }
 
